be39fe99d83e57546b51b68d985c16c035dc1f2a
Kami <josephkiarie561@gmail.com>
2026-02-06 10:44:10 +0300

    X features, y labels and train test split

--- src/models/train.py
+++ src/models/train.py
@@ -5,3 +5,24 @@ from sklearn.metrics import accuracy_score, recall_score
 from xgboost import XGBClassifier
 import mlflow
 import mlflow.xgboost
+
+def train_model(data: pd.DataFrame, target_column:str):
+    """
+    trains an xgboost model and logs with mlflow
+    
+    :param data: Description
+    :type data: pd.DataFrame
+    :param target_column: Description
+    :type target_column: str
+    """
+
+    # X features, y targets
+    X = data.drop(columns=[target_column])
+    y = data[target_column]
+
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size = 0.2,
+        random_state = 21
+    )
